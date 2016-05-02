7d183fd8d6d276dc996af5b0726f3ccceef0581b
WilsonHintz <guillermohintz@gmail.com>
2016-05-02 11:55:59 -0300

    cambios 1 dia bis
    los msmo

--- Generador_Imagenes/Form1.vb
+++ Generador_Imagenes/Form1.vb
@@ -3,35 +3,38 @@
 Public Class Form1
 
     Private Sub btn_generar_Click(sender As Object, e As EventArgs) Handles btn_generar.Click
-        Dim flag As New Bitmap(300, 300)
+        Dim flag As New Bitmap(400, 300)
         Dim pen As New Pen(Color.FromArgb(255, 0, 0, 255))
         Dim flagGraphics As Graphics = Graphics.FromImage(flag)
         Dim path As New GraphicsPath() 'creamos el camino para definir la region
-        Dim points As PointF() = { 'creamos los puntos para definir la region
+
+        Dim rectas As PointF() = { 'creamos los puntos para definir las rectas inferiores
+            New Point(0, 110),
             New Point(0, 300),
-            New Point(50, 80),
-            New Point(100, 20),
-            New Point(150, 80),
-            New Point(200, 100)}
-        path.AddPolygon(points) 'Le damos al path los puntos para armarse
+            New Point(300, 300),
+            New Point(300, 200)}
+
+        Dim horizonte As PointF() = { 'creamos los puntos para definir el horizonte de la tierra
+            New Point(0, 110),
+            New Point(200, 110),
+            New Point(300, 200)}
+
+        'path.AddPolygon(points) 'Le damos al path los puntos para armarse
+        path.AddLines(rectas)
+        path.AddCurve(horizonte, 1.0F)
 
         Dim [region] As New Region(path) 'Construimos la region a partir del path
 
         flagGraphics.DrawPath(pen, path)                    'pintamos el perimetro de la region
         flagGraphics.SetClip(region, CombineMode.Replace)   'seteamos el clip
         flagGraphics.FillRegion(Brushes.Green, region)      'pintamos la region
-
-        'flagGraphics.DrawCurve(pen, points, 1)
-        'flagGraphics.DrawCurve(pen, points, 0.6F)
-        'flagGraphics.DrawCurve(pen, points, 0.2F)
-
-        PictureBox1.Image = flag
+        PictureBox1.Image = flag                            'dibujamos sobre el objeto en la form
         ' flag.Save("lamao.jpg")
 
     End Sub
 
     Public Sub CreateBitmapAtRuntime()
-        PictureBox1.Size = New Size(300, 300)
+        PictureBox1.Size = New Size(400, 400)
         Me.Controls.Add(PictureBox1)
     End Sub
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
