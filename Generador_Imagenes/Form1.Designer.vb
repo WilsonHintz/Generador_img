176fd866896bcaaded86b850f88b7d164bd9d94c
WilsonHintz <guillermohintz@gmail.com>
2016-05-02 11:55:31 -0300

    Cambios 1 dia CIA
    Añadido los campos en el front y la region con horizonte y rectas

--- Generador_Imagenes/Form1.Designer.vb
+++ Generador_Imagenes/Form1.Designer.vb
@@ -24,42 +24,48 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.btn_generar = New System.Windows.Forms.Button()
-        Me.txt_num1 = New System.Windows.Forms.TextBox()
+        Me.x_punto1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.txt_num2 = New System.Windows.Forms.TextBox()
+        Me.y_punto1 = New System.Windows.Forms.TextBox()
+        Me.x_punto2 = New System.Windows.Forms.TextBox()
+        Me.y_punto2 = New System.Windows.Forms.TextBox()
+        Me.x_punto3 = New System.Windows.Forms.TextBox()
+        Me.y_punto3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Location = New System.Drawing.Point(26, 23)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(332, 297)
+        Me.PictureBox1.Size = New System.Drawing.Size(345, 300)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
         'btn_generar
         '
-        Me.btn_generar.Location = New System.Drawing.Point(376, 212)
+        Me.btn_generar.Location = New System.Drawing.Point(425, 272)
         Me.btn_generar.Name = "btn_generar"
         Me.btn_generar.Size = New System.Drawing.Size(126, 51)
         Me.btn_generar.TabIndex = 1
         Me.btn_generar.Text = "Generar"
         Me.btn_generar.UseVisualStyleBackColor = True
         '
-        'txt_num1
+        'x_punto1
         '
-        Me.txt_num1.Location = New System.Drawing.Point(366, 62)
-        Me.txt_num1.Name = "txt_num1"
-        Me.txt_num1.Size = New System.Drawing.Size(50, 20)
-        Me.txt_num1.TabIndex = 2
+        Me.x_punto1.Location = New System.Drawing.Point(432, 62)
+        Me.x_punto1.Name = "x_punto1"
+        Me.x_punto1.Size = New System.Drawing.Size(50, 20)
+        Me.x_punto1.TabIndex = 2
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(363, 27)
+        Me.Label1.Location = New System.Drawing.Point(382, 65)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(44, 13)
         Me.Label1.TabIndex = 3
@@ -68,7 +74,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(363, 46)
+        Me.Label2.Location = New System.Drawing.Point(429, 46)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(12, 13)
         Me.Label2.TabIndex = 4
@@ -77,29 +83,81 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(432, 46)
+        Me.Label3.Location = New System.Drawing.Point(498, 46)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(12, 13)
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "y"
         '
-        'txt_num2
+        'y_punto1
         '
-        Me.txt_num2.Location = New System.Drawing.Point(435, 62)
-        Me.txt_num2.Name = "txt_num2"
-        Me.txt_num2.Size = New System.Drawing.Size(50, 20)
-        Me.txt_num2.TabIndex = 6
+        Me.y_punto1.Location = New System.Drawing.Point(501, 62)
+        Me.y_punto1.Name = "y_punto1"
+        Me.y_punto1.Size = New System.Drawing.Size(50, 20)
+        Me.y_punto1.TabIndex = 6
+        '
+        'x_punto2
+        '
+        Me.x_punto2.Location = New System.Drawing.Point(432, 99)
+        Me.x_punto2.Name = "x_punto2"
+        Me.x_punto2.Size = New System.Drawing.Size(50, 20)
+        Me.x_punto2.TabIndex = 7
+        '
+        'y_punto2
+        '
+        Me.y_punto2.Location = New System.Drawing.Point(501, 99)
+        Me.y_punto2.Name = "y_punto2"
+        Me.y_punto2.Size = New System.Drawing.Size(50, 20)
+        Me.y_punto2.TabIndex = 8
+        '
+        'x_punto3
+        '
+        Me.x_punto3.Location = New System.Drawing.Point(432, 134)
+        Me.x_punto3.Name = "x_punto3"
+        Me.x_punto3.Size = New System.Drawing.Size(50, 20)
+        Me.x_punto3.TabIndex = 9
+        '
+        'y_punto3
+        '
+        Me.y_punto3.Location = New System.Drawing.Point(501, 134)
+        Me.y_punto3.Name = "y_punto3"
+        Me.y_punto3.Size = New System.Drawing.Size(50, 20)
+        Me.y_punto3.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(382, 102)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Punto 2"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(382, 137)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(44, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Punto 3"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(563, 352)
-        Me.Controls.Add(Me.txt_num2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.y_punto3)
+        Me.Controls.Add(Me.x_punto3)
+        Me.Controls.Add(Me.y_punto2)
+        Me.Controls.Add(Me.x_punto2)
+        Me.Controls.Add(Me.y_punto1)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.txt_num1)
+        Me.Controls.Add(Me.x_punto1)
         Me.Controls.Add(Me.btn_generar)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
@@ -112,9 +170,15 @@ Partial Class Form1
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents btn_generar As Button
-    Friend WithEvents txt_num1 As TextBox
+    Friend WithEvents x_punto1 As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents txt_num2 As TextBox
+    Friend WithEvents y_punto1 As TextBox
+    Friend WithEvents x_punto2 As TextBox
+    Friend WithEvents y_punto2 As TextBox
+    Friend WithEvents x_punto3 As TextBox
+    Friend WithEvents y_punto3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
 End Class
