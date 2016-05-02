4445486ee3847877e163750375e01485dbcad5bb
WilsonHintz <guillermohintz@gmail.com>
2016-05-01 22:44:07 -0300

    Primer Push
    archivos originales

---
+++ Generador_Imagenes/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btn_generar = New System.Windows.Forms.Button()
+        Me.txt_num1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt_num2 = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(332, 297)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'btn_generar
+        '
+        Me.btn_generar.Location = New System.Drawing.Point(376, 212)
+        Me.btn_generar.Name = "btn_generar"
+        Me.btn_generar.Size = New System.Drawing.Size(126, 51)
+        Me.btn_generar.TabIndex = 1
+        Me.btn_generar.Text = "Generar"
+        Me.btn_generar.UseVisualStyleBackColor = True
+        '
+        'txt_num1
+        '
+        Me.txt_num1.Location = New System.Drawing.Point(366, 62)
+        Me.txt_num1.Name = "txt_num1"
+        Me.txt_num1.Size = New System.Drawing.Size(50, 20)
+        Me.txt_num1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(363, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Punto 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(363, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(12, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "x"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(432, 46)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(12, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "y"
+        '
+        'txt_num2
+        '
+        Me.txt_num2.Location = New System.Drawing.Point(435, 62)
+        Me.txt_num2.Name = "txt_num2"
+        Me.txt_num2.Size = New System.Drawing.Size(50, 20)
+        Me.txt_num2.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(563, 352)
+        Me.Controls.Add(Me.txt_num2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_num1)
+        Me.Controls.Add(Me.btn_generar)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btn_generar As Button
+    Friend WithEvents txt_num1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txt_num2 As TextBox
+End Class
---
+++ Generador_Imagenes/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Drawing.Drawing2D
+
+Public Class Form1
+
+    Private Sub btn_generar_Click(sender As Object, e As EventArgs) Handles btn_generar.Click
+        Dim flag As New Bitmap(300, 300)
+        Dim pen As New Pen(Color.FromArgb(255, 0, 0, 255))
+        Dim flagGraphics As Graphics = Graphics.FromImage(flag)
+        Dim path As New GraphicsPath() 'creamos el camino para definir la region
+        Dim points As PointF() = { 'creamos los puntos para definir la region
+            New Point(0, 300),
+            New Point(50, 80),
+            New Point(100, 20),
+            New Point(150, 80),
+            New Point(200, 100)}
+        path.AddPolygon(points) 'Le damos al path los puntos para armarse
+
+        Dim [region] As New Region(path) 'Construimos la region a partir del path
+
+        flagGraphics.DrawPath(pen, path)                    'pintamos el perimetro de la region
+        flagGraphics.SetClip(region, CombineMode.Replace)   'seteamos el clip
+        flagGraphics.FillRegion(Brushes.Green, region)      'pintamos la region
+
+        'flagGraphics.DrawCurve(pen, points, 1)
+        'flagGraphics.DrawCurve(pen, points, 0.6F)
+        'flagGraphics.DrawCurve(pen, points, 0.2F)
+
+        PictureBox1.Image = flag
+        flag.Save("lamao.jpg")
+
+    End Sub
+
+    Public Sub CreateBitmapAtRuntime()
+        PictureBox1.Size = New Size(300, 300)
+        Me.Controls.Add(PictureBox1)
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CreateBitmapAtRuntime()
+    End Sub
+
+End Class
